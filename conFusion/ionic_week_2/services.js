ddd374ac3df45112460d647ccc579dd5ddd71ec3
Panaska <boojum1@yandex.ru>
2017-05-11 16:48:31 +0300

    Add files via upload

---
+++ conFusion/ionic_week_2/services.js
@@ -0,0 +1,73 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+        .constant("baseURL","http://localhost:3000/")
+		
+        .factory('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {           
+    
+               // this.getDishes = function(){
+                    
+            return $resource(baseURL+"dishes/:id",null,  {		'update':{
+						method:'PUT' 
+					}
+					
+				});
+                    
+           //     };
+    }])
+                // implement a function named getPromotion
+                // that returns a selected promotion.
+				
+	.factory('promotionFactory', ['$resource', 'baseURL', function($resource,baseURL) { 
+	
+               // this.getPromotion = function() {
+             return $resource(baseURL+"promotions/:id");
+              //  }
+    
+                        
+        }])
+
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+    
+            return $resource(baseURL+"leadership/:id");
+    
+        }])
+
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {    
+    
+            return $resource(baseURL+"feedback/:id");
+    
+        }])
+		
+		.service('favoriteFactory', ['$resource', 'baseURL', function($resource,baseURL) {    
+              
+				var favFac = {};
+				var favorites = [];
+				
+                favFac.addToFavorites = function (index) {
+        for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index)
+                return;
+        }
+        favorites.push({id: index});
+		
+    };    
+	
+	 favFac.deleteFromFavorites = function (index) {
+        for (var i = 0; i < favorites.length; i++) {
+            if (favorites[i].id == index) {
+                favorites.splice(i, 1);
+            }
+        }
+    }
+
+    favFac.getFavorites = function () {
+        return favorites;
+    };
+
+                
+                return favFac;        
+        }])
+
+;
